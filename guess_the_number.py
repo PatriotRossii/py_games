3d8dbf26f6375c019ad91f0301049cd953bd6a2c
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-17 15:16:21 +0500

    Add constant TRIES_COUNT

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,16 +3,17 @@
 import random
 
 number = random.randint(1, 20) # Рандом 1-20
+TRIES_COUNT = 7 # Константа, начальное количество попыток
 
 print('!', number) # Загаданное число
 
-for i in range(7):
+for i in range(TRIES_COUNT):
     user = int(input())
     
     if user > number:
-        print('Загаданое число меньше. Осталось попыток:', 6-i)
-    if user < number:\
-        print('Загаданое число больше. Осталось попыток:', 6-i)
+        print('Загаданое число меньше. Осталось попыток:', TRIES_COUNT - 1 - i)
+    if user < number:
+        print('Загаданое число больше. Осталось попыток:', TRIES_COUNT - 1 - i)
     if user == number:
         print('Угадал - ', number)
         break
